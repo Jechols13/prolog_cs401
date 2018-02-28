5c08fd9e84401f531df4a80e7149e5c1076320b9
Branwin DuBose <bdubose@doozer.com>
2018-02-28 15:36:21 -0600

    some work, trying to read as a string from console

--- main.pl
+++ main.pl
@@ -5,23 +5,42 @@
 main :-
     prompt1(">"),
     read(X),
+    write(X),
+    term_string(X, STR),
+    string_length(STR, LEN),
+    LEN_MINUS_ONE is LEN-1,
+    sub_string(STR, LEN_MINUS_ONE, 1, 0, PUNCT),
+    write(PUNCT).
+
+    %(PUNCT is "." ->
     % the next part will be either input or query,
     % We will base this on 
     % input
-    atomic_list_concat([N1, N2], ' is a ', X),
-    assert(is_a(N1, N2)),
-    write("Ok.");
+    %atomic_list_concat([N1, N2], ' is a ', X),
+    %assert(is_a(N1, N2)),
+    %write("Ok."),
+    %nl,
+    %main;
 
-
+    %PUNCT is "?" ->
     % query
     % We'll need to write one of these as a response, to be changed.
     %write("Yes."); write("No."); write("Unknown."),
-    nl,
-    main. 
+    %nl,
+    %main). 
 
 
 last_char(STR) :-
-    string_length(STR, X),
-    LEN_MINUS_ONE is X -1,
-    sub_string(STR, LEN_MINUS_ONE, 1, 0, Y),
+    string_length(STR, LEN),
+    LEN_MINUS_ONE is LEN -1,
+    sub_string(STR, LEN_MINUS_ONE, 1, 0, PUNCT),
     write(Y).
+
+str_test() :-
+    read_line_to_codes(user_input, X),
+    (string(X) ->
+        write("X is a string.");
+     compound(X) ->
+        write(X);
+     atom(X) ->
+        write("X is atom.")).
