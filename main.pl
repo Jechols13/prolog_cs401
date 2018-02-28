23d16f3a6c9e500666f0ec93713f96c01dd608be
Branwin DuBose <bdubose@doozer.com>
2018-02-28 15:09:45 -0600

    added last_char predicate to main.pl, just as an example, I think we'll have to just copy paste this into main

--- main.pl
+++ main.pl
@@ -2,17 +2,26 @@
 % is_a is a dynamic rule that take two aruments.
 :- dynamic is_a/2.
 
-main_loop :-
-    %prompt1(">"),
+main :-
+    prompt1(">"),
     read(X),
     % the next part will be either input or query,
     % We will base this on 
     % input
+    atomic_list_concat([N1, N2], ' is a ', X),
+    assert(is_a(N1, N2)),
     write("Ok.");
 
 
     % query
     % We'll need to write one of these as a response, to be changed.
-    write("Yes."); write("No."); write("Unknown."),
+    %write("Yes."); write("No."); write("Unknown."),
     nl,
-    main_loop.
+    main. 
+
+
+last_char(STR) :-
+    string_length(STR, X),
+    LEN_MINUS_ONE is X -1,
+    sub_string(STR, LEN_MINUS_ONE, 1, 0, Y),
+    write(Y).
