3f7d2c163ad8c7c8d0bb53fea781f91124d9070c
Jechols <jecho@Joshuas-MacBook-Pro.local>
2018-03-12 07:38:28 -0500

    Cleaning up. Adding more keywords

--- psychiatrist.pl
+++ psychiatrist.pl
@@ -11,28 +11,27 @@ main :-
 
 keep_going :- %figure out a way to loop this and keep reading in input
         read(Y),
-        string_lower(Y,Lowercase), 
+        string_lower(Y,Lowercase),
         split_string(Lowercase, ' ',' ', List),
-        (read_stmts(List); write("I dont understand. Tell me more."),nl),
+        (read_stmts(List),nl; write("I dont understand. Tell me more."),nl),
         keep_going.
 
-read_stmts(List) :- 
-                %will change to while loop 
-               ((nth0(Index, List, X, R),nth0(Index, R, Y, R1)); %find first and second word (if it exists)
-                nth0(Index, List, X, R)), %finds first word.
-                (((X == "this"), (Y == "is")) -> write('What else do you regard as too much?'), nl, keep_going;      
-                ((X == "mother"); (X == "father"); (X == "sister"); (X == "brother")) -> write('Tell me more about your family'), nl, keep_going;
-                (X == "why") -> write('Why should you what?'), nl, keep_going;
-                (X == "good") -> write('Thats good to hear!'), nl, keep_going;
-                (X == "bad") -> write('Will food make it better?'), nl, keep_going;
-                (X == "yes") -> write('Perfect!'), nl, keep_going;
-                (X == "no") -> write('Sorry I couldnt help'), nl, keep_going;
-                (X == "sick") -> write('Get well soon.'), nl, keep_going;
-                (X == "thank"); (X == "thanks") -> write('My pleasure.'), nl, keep_going;
-                (X == "quit") ->write('Nice conversation, chat later!'), nl, halt()); 
-
-                % if X not the end of list continue, otherwise propmt for more input
-                (not(last(R,X)), read_stmts(R)). 
-
-
-    
+read_stmts(List) :-
+        ((nth0(Index, List, X, R),nth0(Index, R, Y, R1)); %find first and second word (if it exists) OR
+        nth0(Index, List, X, R)), %finds first word. AND
+        (
+                ((X == "this"), (Y == "is")) -> write('What else do you regard as too much?');
+                ((X == "mother"); (X == "father"); (X == "sister"); (X == "brother")) -> write('Tell me more about your family');
+                (X == "why") -> write('Why should you what?');
+                (X == "good") -> write('Thats good to hear!');
+                (X == "bad") -> write('Will food make it better?');
+                (X == "yes") -> write('Perfect!');
+                (X == "birthday") -> write('Happy birthday!');
+                (X == "beautiful") -> write('Looks can be decieving someimes.');
+                (X == "no") -> write('Sorry I couldnt help');
+                (X == "sick") -> write('Get well soon.');
+                ((X == "thank"); (X == "thanks")) -> write('My pleasure.');
+                (X == "quit") ->write('Nice conversation, chat later!'), nl, halt()
+        );
+        % if X not the end of list continue, otherwise propmt for more input
+        (not(last(R,X)), read_stmts(R)).
