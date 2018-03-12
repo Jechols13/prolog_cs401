5656112f15db96d5de1dfb4d3721fcedfee72891
Jechols <jecho@Joshuas-MacBook-Pro.local>
2018-03-11 22:00:46 -0500

    Able to read many responses. May add more keywords as we wish

--- psychiatrist.pl
+++ psychiatrist.pl
@@ -10,12 +10,14 @@ main :-
 keep_going :- %figure out a way to loop this and keep reading in input
         read(Y), 
         split_string(Y, ' ',' ', List),
-        read_stmts(List,_),
+        (read_stmts(List); write("I dont understand. Tell me more."),nl),
         keep_going.
 
-read_stmts(List, Y) :- 
+read_stmts(List) :- 
                 %will change to while loop 
-                nth0(0, List, X, R), %indexing through list. I is index, list, res, rest of list.
+                (nth0(A, List, X, R), %finds first index
+                not(last(List,X)), %make sure not at end of list (if we want second word)
+                nth0(B, R, Y, R1), %finds second word.
                 ((X = "this"), (Y = "is") -> write('What else do you regard as too much?'), nl, keep_going;      
                 ((X = "mother"); (X = "father"); (X = "sister"); (X = "brother")) -> write('Tell me more about your family'), nl, keep_going;
                 (X = "why") -> write('Why should you what?'), nl, keep_going;
@@ -24,11 +26,23 @@ read_stmts(List, Y) :-
                 (X = "yes") -> write('Perfect!'), nl, keep_going;
                 (X = "no") -> write('Sorry I couldnt help'), nl, keep_going;
                 (X = "hello"); (X = "hey"); (X = "hi") -> write('Hello, how are you?'), nl, keep_going;
-                (X = "quit") ->write('Nice conversation, chat later!'), nl, halt();
-                write("I don't understand. Tell me more."), nl, keep_going).
+                (X = "quit") ->write('Nice conversation, chat later!'), nl, halt())); 
 
+                % if there is a one word response then do this. (Probably a better way to do this but it works)
 
+                (nth0(A, List, X, R), %finds first index
+                ((X = "this"), (Y = "is") -> write('What else do you regard as too much?'), nl, keep_going;      
+                ((X = "mother"); (X = "father"); (X = "sister"); (X = "brother")) -> write('Tell me more about your family'), nl, keep_going;
+                (X = "why") -> write('Why should you what?'), nl, keep_going;
+                (X = "good") -> write('Thats good to hear!'), nl, keep_going;
+                (X = "bad") -> write('Will food make it better?'), nl, keep_going;
+                (X = "yes") -> write('Perfect!'), nl, keep_going;
+                (X = "no") -> write('Sorry I couldnt help'), nl, keep_going;
+                (X = "hello"); (X = "hey"); (X = "hi") -> write('Hello, how are you?'), nl, keep_going;
+                (X = "quit") ->write('Nice conversation, chat later!'), nl, halt()));
 
+                %prevents endless loop. X in R can not be the last element, if so we have exhausted all searches
+                (not(last(R,X)), read_stmts(R)). 
 
 
     
