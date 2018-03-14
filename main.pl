cd1aacc2ff6442d98dbf4d19a15d345dab3dd624
Branwin DuBose <bdubose@doozer.com>
2018-03-14 15:25:27 -0500

    HA! can now do A is a B is a Cgit status! :D

--- main.pl
+++ main.pl
@@ -1,9 +1,15 @@
 
 % is_a is a dynamic rule that take two aruments.
 :- dynamic is_a/2.
-% TODO: need a way to check if x is a z by finding a y so that x is a y and y is a z.
 
-new_main :-
+% is A a B?
+check_is_a(A, B) :- is_a(A, B).
+% is A a B that is a C?
+check_is_a(A, C) :-
+    is_a(A, B),
+    is_a(B, C).
+
+main :-
     write("> "),
     read_line_to_codes(user_input, Input),
     deleteLastElement(Input, WithoutPunc),
@@ -12,10 +18,10 @@ new_main :-
         type_is_query(Input) -> quer(IA)
         ; stmt(IA)
     ),
-    new_main.
+    main.
 
 
-stmt(IA) :- % TODO: still need to split off last period
+stmt(IA) :-
     atomic_list_concat(List_atoms,' is a ',IA),     % split on ' is a '
     List_atoms = [First | _],                       % First = the first in the list
     string_to_atom(Str, First),                     % convert First into string
@@ -26,23 +32,27 @@ stmt(IA) :- % TODO: still need to split off last period
     write("Ok."), nl.                                   % Print ok
 
 
-quer(IA) :- % TODO: still need split off last period 
+quer(IA) :-
     atomic_list_concat(List_atoms, 'Is a ', IA),    % split into list based on 'Is a '
     atomic_list_concat(List_atoms, '' , EndStr),    % glue back into string
     string_to_atom(EndStr, SplitMe),                % convert string into atom
     atomic_list_concat(Split, ' a ', SplitMe),      % split into list around ' a ' 
     Split = [First, Last],                          % Assign separate variables
     (
-        is_a(First, Last) -> write("Yes."), nl
+        check_is_a(First, Last) -> write("Yes."), nl
         ; write("No."), nl
     ).
 
+% define what an 'I/i' is
 is_I(73).
 is_I(105).
+
+% remove last element (for removing punctuation from my input (?/.))
 deleteLastElement([_], []).
 deleteLastElement([Head, Next|Tail], [Head|NTail]):-
   deleteLastElement([Next|Tail], NTail).
 
+% Since all queries begin with the word "is", I will check if the first letter is an "i" or not
 type_is_query(Input) :-
     Input = [FirstCode | _],
     is_I(FirstCode).
