0a881444af9e60c0f34dac03bed91ad79dbf75be
Branwin DuBose <bdubose@doozer.com>
2018-03-15 21:24:07 -0500

    accidentally removed that too soon

--- main.pl
+++ main.pl
@@ -33,7 +33,7 @@ stmt(IA) :-
 
 
 quer(IA) :-
-    atomic_list_concat(List_atoms, 'Is ', IA),    % split into list based on 'Is a '
+    atomic_list_concat(List_atoms, 'Is a ', IA),    % split into list based on 'Is a '
     atomic_list_concat(List_atoms, '' , EndStr),    % glue back into string
     string_to_atom(EndStr, SplitMe),                % convert string into atom
     atomic_list_concat(Split, ' a ', SplitMe),      % split into list around ' a ' 
